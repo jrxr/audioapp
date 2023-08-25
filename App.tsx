dc5c1d6787c6d39343703acead613fb415602b1a
agacy junior <juniormartinssom@gmail.com>
2023-08-25 09:37:43 -0300

    Finish project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Text, View, Pressable, Alert, Button } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
+
+import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from "expo-av";
 
 export default function App() {
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+  const [recordingFileURI, setRecordingFileURI] = useState<string | null>(null);
+
+  async function handleRecordingStart() {
+    const { granted } = await Audio.getPermissionsAsync();
+
+    if (granted) {
+      try {
+        const { recording } = await Audio.Recording.createAsync();
+        setRecording(recording);
+      } catch (error) {
+        console.log(error);
+        Alert.alert(
+          "Erro ao gravar",
+          "Não foi possível iniciar a gravação do áudio."
+        );
+      }
+    }
+  }
+
+  async function handleRecordingStop() {
+    try {
+      if (recording) {
+        await recording.stopAndUnloadAsync();
+        const fileUri = recording.getURI();
+        console.log(fileUri);
+        setRecordingFileURI(fileUri);
+        setRecording(null);
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Erro ao pausar",
+        "Não foi possível parar a gravação do áudio."
+      );
+    }
+  }
+
+  async function handleAudioPlay() {
+    if (recordingFileURI) {
+      const { sound } = await Audio.Sound.createAsync(
+        {
+          uri: recordingFileURI,
+        },
+        {
+          shouldPlay: true,
+        }
+      );
+
+      await sound.setPositionAsync(0);
+      await sound.playAsync();
+    }
+  }
+
+  useEffect(() => {
+    Audio.requestPermissionsAsync().then(({ granted }) => {
+      if (granted) {
+        Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+          playsInSilentModeIOS: true,
+          shouldDuckAndroid: true,
+          interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+          playThroughEarpieceAndroid: true,
+        });
+      }
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        style={[styles.button, recording && styles.recording]}
+        onPressIn={handleRecordingStart}
+        onPressOut={handleRecordingStop}
+      >
+        <MaterialIcons name="mic" size={45} color="#212121" />
+      </Pressable>
+
+      {recording && <Text style={styles.label}>Gravando</Text>}
+
+      {recordingFileURI && (
+        <Button title="Ouvir Áudio" onPress={handleAudioPlay} />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
